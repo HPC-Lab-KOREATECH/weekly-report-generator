52af90000aa8917beb14a6617fc7f50816f95e6e
refracta <58779799+refracta@users.noreply.github.com>
2023-07-01 04:15:52 +0900

    Minor fix

--- index.js
+++ index.js
@@ -136,5 +136,5 @@ const generateWeeklyMarkdown = async(pptx) => {
 }
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Server is listening on port 3000');
+    console.log(`Server is listening on port ${process.env.PORT || 3000}`);
 });
